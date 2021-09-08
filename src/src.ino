3547e0002efa42f29f15370af13254d9673f6bf8
dani <danieljerezgarrido@gmail.com>
2021-09-08 17:43:27 +0200

    format

--- src/src.ino
+++ src/src.ino
@@ -54,8 +54,8 @@ bool alarmWentOff = false;
 
 
 /**
- * Get delay between particle drops (in seconds)
- */
+   Get delay between particle drops (in seconds)
+*/
 long getDelayDrop() {
   // since we have exactly 60 particles we don't have to multiply by 60 and then divide by the number of particles again :)
   return delayMinutes + delayHours * 60;
@@ -65,16 +65,16 @@ long getDelayDrop() {
 #if DEBUG_OUTPUT
 void printmatrix() {
   Serial.println(" 0123-4567 ");
-  for (int y = 0; y<8; y++) {
+  for (int y = 0; y < 8; y++) {
     if (y == 4) {
       Serial.println("|----|----|");
     }
     Serial.print(y);
-    for (int x = 0; x<8; x++) {
+    for (int x = 0; x < 8; x++) {
       if (x == 4) {
         Serial.print("|");
       }
-      Serial.print(lc.getXY(0,x,y) ? "X" :" ");
+      Serial.print(lc.getXY(0, x, y) ? "X" : " ");
     }
     Serial.println("|");
   }
@@ -86,20 +86,20 @@ void printmatrix() {
 
 coord getDown(int x, int y) {
   coord xy;
-  xy.x = x-1;
-  xy.y = y+1;
+  xy.x = x - 1;
+  xy.y = y + 1;
   return xy;
 }
 coord getLeft(int x, int y) {
   coord xy;
-  xy.x = x-1;
+  xy.x = x - 1;
   xy.y = y;
   return xy;
 }
 coord getRight(int x, int y) {
   coord xy;
   xy.x = x;
-  xy.y = y+1;
+  xy.y = y + 1;
   return xy;
 }
 
@@ -125,22 +125,22 @@ bool canGoDown(int addr, int x, int y) {
 
 void goDown(int addr, int x, int y) {
   lc.setXY(addr, x, y, false);
-  lc.setXY(addr, getDown(x,y), true);
+  lc.setXY(addr, getDown(x, y), true);
 }
 void goLeft(int addr, int x, int y) {
   lc.setXY(addr, x, y, false);
-  lc.setXY(addr, getLeft(x,y), true);
+  lc.setXY(addr, getLeft(x, y), true);
 }
 void goRight(int addr, int x, int y) {
   lc.setXY(addr, x, y, false);
-  lc.setXY(addr, getRight(x,y), true);
+  lc.setXY(addr, getRight(x, y), true);
 }
 
 
 int countParticles(int addr) {
   int c = 0;
-  for (byte y=0; y<8; y++) {
-    for (byte x=0; x<8; x++) {
+  for (byte y = 0; y < 8; y++) {
+    for (byte x = 0; x < 8; x++) {
       if (lc.getXY(addr, x, y)) {
         c++;
       }
@@ -151,7 +151,7 @@ int countParticles(int addr) {
 
 
 bool moveParticle(int addr, int x, int y) {
-  if (!lc.getXY(addr,x,y)) {
+  if (!lc.getXY(addr, x, y)) {
     return false;
   }
 
@@ -166,7 +166,7 @@ bool moveParticle(int addr, int x, int y) {
 
   if (can_GoDown) {
     goDown(addr, x, y);
-  } else if (can_GoLeft&& !can_GoRight) {
+  } else if (can_GoLeft && !can_GoRight) {
     goLeft(addr, x, y);
   } else if (can_GoRight && !can_GoLeft) {
     goRight(addr, x, y);
@@ -182,13 +182,13 @@ bool moveParticle(int addr, int x, int y) {
 
 void fill(int addr, int maxcount) {
   int n = 8;
-  byte x,y;
+  byte x, y;
   int count = 0;
-  for (byte slice = 0; slice < 2*n-1; ++slice) {
-    byte z = slice<n ? 0 : slice-n + 1;
-    for (byte j = z; j <= slice-z; ++j) {
-      y = 7-j;
-      x = (slice-j);
+  for (byte slice = 0; slice < 2 * n - 1; ++slice) {
+    byte z = slice < n ? 0 : slice - n + 1;
+    for (byte j = z; j <= slice - z; ++j) {
+      y = 7 - j;
+      x = (slice - j);
       lc.setXY(addr, x, y, (++count <= maxcount));
     }
   }
@@ -197,21 +197,29 @@ void fill(int addr, int maxcount) {
 
 
 /**
- * Detect orientation using the accelerometer
- *
- *     | up | right | left | down |
- * --------------------------------
- * 400 |    |       | y    | x    |
- * 330 | y  | x     | x    | y    |
- * 260 | x  | y     |      |      |
- */
+   Detect orientation using the accelerometer
+
+       | up | right | left | down |
+   --------------------------------
+   400 |    |       | y    | x    |
+   330 | y  | x     | x    | y    |
+   260 | x  | y     |      |      |
+*/
 int getGravity() {
   int x = mpu6050.getAngleX();
   int y = mpu6050.getAngleY();
-  if (y < ACC_THRESHOLD_LOW)  { return 90;   }
-  if (x > ACC_THRESHOLD_HIGH) { return 0;  }
-  if (y > ACC_THRESHOLD_HIGH) { return 270; }
-  if (x < ACC_THRESHOLD_LOW)  { return 180; }
+  if (y < ACC_THRESHOLD_LOW)  {
+    return 90;
+  }
+  if (x > ACC_THRESHOLD_HIGH) {
+    return 0;
+  }
+  if (y > ACC_THRESHOLD_HIGH) {
+    return 270;
+  }
+  if (x < ACC_THRESHOLD_LOW)  {
+    return 180;
+  }
 }
 
 
@@ -225,7 +233,7 @@ int getBottomMatrix() {
 
 
 void resetTime() {
-  for (byte i=0; i<2; i++) {
+  for (byte i = 0; i < 2; i++) {
     lc.clearDisplay(i);
   }
   fill(getTopMatrix(), 60);
@@ -235,19 +243,19 @@ void resetTime() {
 
 
 /**
- * Traverse matrix and check if particles need to be moved
- */
+   Traverse matrix and check if particles need to be moved
+*/
 bool updateMatrix() {
   int n = 8;
   bool somethingMoved = false;
-  byte x,y;
+  byte x, y;
   bool direction;
-  for (byte slice = 0; slice < 2*n-1; ++slice) {
+  for (byte slice = 0; slice < 2 * n - 1; ++slice) {
     direction = (random(2) == 1); // randomize if we scan from left to right or from right to left, so the grain doesn't always fall the same direction
-    byte z = slice<n ? 0 : slice-n + 1;
-    for (byte j = z; j <= slice-z; ++j) {
-      y = direction ? (7-j) : (7-(slice-j));
-      x = direction ? (slice-j) : j;
+    byte z = slice < n ? 0 : slice - n + 1;
+    for (byte j = z; j <= slice - z; ++j) {
+      y = direction ? (7 - j) : (7 - (slice - j));
+      x = direction ? (slice - j) : j;
       // for (byte d=0; d<2; d++) { lc.invertXY(0, x, y); delay(50); }
       if (moveParticle(MATRIX_B, x, y)) {
         somethingMoved = true;
@@ -263,15 +271,15 @@ bool updateMatrix() {
 
 
 /**
- * Let a particle go from one matrix to the other
- */
+   Let a particle go from one matrix to the other
+*/
 boolean dropParticle() {
   if (d.Timeout()) {
     d.Delay(getDelayDrop() * 1000);
     if (gravity == 0 || gravity == 180) {
       if ((lc.getRawXY(MATRIX_A, 0, 0) && !lc.getRawXY(MATRIX_B, 7, 7)) ||
           (!lc.getRawXY(MATRIX_A, 0, 0) && lc.getRawXY(MATRIX_B, 7, 7))
-      ) {
+         ) {
         // for (byte d=0; d<8; d++) { lc.invertXY(0, 0, 7); delay(50); }
         lc.invertRawXY(MATRIX_A, 0, 0);
         lc.invertRawXY(MATRIX_B, 7, 7);
@@ -286,7 +294,7 @@ boolean dropParticle() {
 
 
 void alarm() {
-  for (int i=0; i<5; i++) {
+  for (int i = 0; i < 5; i++) {
     tone(PIN_BUZZER, 440, 200);
     delay(1000);
   }
@@ -315,24 +323,24 @@ void displayLetter(char letter, int matrix) {
   // Serial.print("Letter: ");
   // Serial.println(letter);
   lc.clearDisplay(matrix);
-  lc.setXY(matrix, 1,4, true);
-  lc.setXY(matrix, 2,3, true);
-  lc.setXY(matrix, 3,2, true);
-  lc.setXY(matrix, 4,1, true);
+  lc.setXY(matrix, 1, 4, true);
+  lc.setXY(matrix, 2, 3, true);
+  lc.setXY(matrix, 3, 2, true);
+  lc.setXY(matrix, 4, 1, true);
 
-  lc.setXY(matrix, 3,6, true);
-  lc.setXY(matrix, 4,5, true);
-  lc.setXY(matrix, 5,4, true);
-  lc.setXY(matrix, 6,3, true);
+  lc.setXY(matrix, 3, 6, true);
+  lc.setXY(matrix, 4, 5, true);
+  lc.setXY(matrix, 5, 4, true);
+  lc.setXY(matrix, 6, 3, true);
 
   if (letter == 'M') {
-    lc.setXY(matrix, 4,2, true);
-    lc.setXY(matrix, 4,3, true);
-    lc.setXY(matrix, 5,3, true);
+    lc.setXY(matrix, 4, 2, true);
+    lc.setXY(matrix, 4, 3, true);
+    lc.setXY(matrix, 5, 3, true);
   }
   if (letter == 'H') {
-    lc.setXY(matrix, 3,3, true);
-    lc.setXY(matrix, 4,4, true);
+    lc.setXY(matrix, 3, 3, true);
+    lc.setXY(matrix, 4, 4, true);
   }
 }
 
@@ -353,10 +361,10 @@ void renderSetHours() {
 void knobClockwise() {
   Serial.println("Clockwise");
   if (mode == MODE_SETHOURS) {
-    delayHours = constrain(delayHours+1, 0, 64);
+    delayHours = constrain(delayHours + 1, 0, 64);
     renderSetHours();
-  } else if(mode == MODE_SETMINUTES) {
-    delayMinutes = constrain(delayMinutes+1, 0, 64);
+  } else if (mode == MODE_SETMINUTES) {
+    delayMinutes = constrain(delayMinutes + 1, 0, 64);
     renderSetMinutes();
   }
   Serial.print("Delay: ");
@@ -365,10 +373,10 @@ void knobClockwise() {
 void knobCounterClockwise() {
   Serial.println("Counterclockwise");
   if (mode == MODE_SETHOURS) {
-    delayHours = constrain(delayHours-1, 0, 64);
+    delayHours = constrain(delayHours - 1, 0, 64);
     renderSetHours();
   } else if (mode == MODE_SETMINUTES) {
-    delayMinutes = constrain(delayMinutes-1, 0, 64);
+    delayMinutes = constrain(delayMinutes - 1, 0, 64);
     renderSetMinutes();
   }
   Serial.print("Delay: ");
@@ -385,11 +393,11 @@ void updateEncoder() {
   int MSB = digitalRead(PIN_ENC_1); //MSB = most significant bit
   int LSB = digitalRead(PIN_ENC_2); //LSB = least significant bit
 
-  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
   int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue--;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue++;
+  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue--;
+  if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue++;
 
   // Serial.print("Value: ");
   // Serial.println(encoderValue);
@@ -405,13 +413,13 @@ void updateEncoder() {
 
 
 /**
- * Button callback (incl. software debouncer)
- * This switches between the modes (normal, set minutes, set hours)
- */
+   Button callback (incl. software debouncer)
+   This switches between the modes (normal, set minutes, set hours)
+*/
 volatile unsigned long lastButtonPushMillis;
 void buttonPush() {
-  if((long)(millis() - lastButtonPushMillis) >= DEBOUNCE_THRESHOLD) {
-    mode = (mode+1) % 3;
+  if ((long)(millis() - lastButtonPushMillis) >= DEBOUNCE_THRESHOLD) {
+    mode = (mode + 1) % 3;
     Serial.print("Switched mode to: ");
     Serial.println(mode);
     lastButtonPushMillis = millis();
@@ -435,14 +443,14 @@ void buttonPush() {
 
 
 /**
- * Setup
- */
+   Setup
+*/
 void setup() {
-mpu6050.calcGyroOffsets(true);
- Serial.begin(9600);
- Wire.begin();
-mpu6050.begin();
-  
+  mpu6050.calcGyroOffsets(true);
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+
   // while (!Serial) {
   //   ; // wait for serial port to connect. Needed for native USB
   // }
@@ -465,9 +473,9 @@ mpu6050.begin();
   randomSeed(analogRead(A0));
 
   // init displays
-  for (byte i=0; i<2; i++) {
-    lc.shutdown(i,false);
-    lc.setIntensity(i,0);
+  for (byte i = 0; i < 2; i++) {
+    lc.shutdown(i, false);
+    lc.setIntensity(i, 0);
   }
 
   resetTime();
@@ -476,17 +484,17 @@ mpu6050.begin();
 
 
 /**
- * Main loop
- */
+   Main loop
+*/
 void loop() {
   mpu6050.update();
   Serial.println("angleX : ");
   Serial.println(mpu6050.getAngleX());
   Serial.println("\tangleY : ");
   Serial.println(mpu6050.getAngleY());
- 
+
   delay(DELAY_FRAME);
-  
+
 
   // update the driver's rotation setting. For the rest of the code we pretend "down" is still 0,0 and "up" is 7,7
   gravity = getGravity();
